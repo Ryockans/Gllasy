e164db414c91907c7635ea15b21b2da60266c1a6
ryockans <front6@front6>
2022-05-06 19:56:30 +0500

    initial

---
+++ src/assets/js/slider.js
@@ -0,0 +1,17 @@
+//Управление слайдерами
+let slideIndex = 1;
+let slideButtons = document.querySelectorAll(".slider__controls-button");
+let slides = document.querySelectorAll(".slider__item");
+let indexBody = document.querySelector(".index-body"); 
+
+function showSlides(n) {
+    if (n !== slideIndex){
+        slideButtons[slideIndex - 1].classList.remove("-current");
+        slideButtons[n - 1].classList.add("-current");
+        slides[slideIndex - 1].classList.remove("-current");
+        slides[n - 1].classList.add("-current");
+        indexBody.classList.replace("site-wrapper-"+ slideIndex, "site-wrapper-"+ n );
+        slideIndex = n; 
+    }
+}
+
